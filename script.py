Local Energy calculation is wrong...

script.py
@@ -21,33 +21,26 @@ hermite_coeffs = [
     [12, 0, -48, 0, 16],       # n=4
     [0, 120, 0, -160, 0, 32],  # n=5
     [-120, 0, 720, 0, -480, 0, 64]  ] # n=6
+polys = len(hermite_coeffs)
 
 #%% 2.1
 
 # Numerical evalutation of the local energies with both 2nd and 4th order truncation:
 num_hermite = len(hermite_coeffs)
+x_example = np.linspace(-4, 4, 1000)
 
-x_vals_second_le = []
-local_energy_second_vals = []
-x_vals_fourth_le = []
-local_energy_fourth_vals = []
-
-for i in range(num_hermite):
-    x_vals_le_second_i, local_energy_vals_second_i = le.local_energy(0.01,
-                                [-1, 1], 1000, hermite_coeffs, i, 0, plot=True)
-    x_vals_second_le.append(x_vals_le_second_i)
-    local_energy_second_vals.append(local_energy_vals_second_i)
+x_vals_le = []
+local_energy_vals = []
 
 for i in range(num_hermite):
-    x_vals_le_fourth_i, local_energy_vals_fourth_i = le.local_energy(0.01,
-                                [-1, 1], 1000, hermite_coeffs, i, 1, plot=True)
-    x_vals_fourth_le.append(x_vals_le_fourth_i)
-    local_energy_fourth_vals.append(local_energy_vals_fourth_i)
+    for j in range(5):
+        x_vals_le_second_i, local_energy_vals_second_i = le.local_energy(x_example, 0.01,
+                                                        hermite_coeffs, i, i, plot=True)
+        x_vals_le.append(x_vals_le_second_i)
+        local_energy_vals.append(local_energy_vals_second_i)
 
-x_vals_second_le = np.array(x_vals_second_le)
-local_energy_second_vals = np.array(local_energy_second_vals)
-x_vals_fourth_le = np.array(x_vals_fourth_le)
-local_energy_fourth_vals = np.array(local_energy_fourth_vals)
+x_vals_le = np.array(x_vals_le)
+local_energy_vals = np.array(local_energy_vals)
 
 # Calculating the RMS error on these calculations:
 
@@ -85,11 +78,21 @@ samples_gaussian_mh_3d = samp.metropolis_hastings_3d(pdfs.gaussian_3d, [0, 0, 0]
 samp.plot_3d_samples(samples_gaussian_r_3d, 40, 0)
 samp.plot_3d_samples(samples_gaussian_mh_3d, 40, 1)
 
-polys = len(hermite_coeffs)
 
+#%% Passing through the local energy function:
+wf_samples = []
 for n in range(polys):
     pdf_qo = partial(pdfs.wf_pdf, n=n, coeffs=hermite_coeffs)
-    samples_wf_r = samp.rejection(pdf_qo, 0, -5, 5.1, 10000, 100000, m=None)
-    samp.plot_samples(pdf_qo, [-5,5.1], samples_wf_r, 150, 0)
+    samples_wf_r = samp.rejection(pdf_qo, 0, -3, 3.1, 10000, 1000000, m=None)
+    #samp.plot_samples(pdf_qo, [-5,5.1], samples_wf_r, 150, 0)
+    wf_samples.append(samples_wf_r)
+
+energies = []
+for i in range(polys):
+    x_values, local_energies = le.local_energy(wf_samples[i], 0.01, hermite_coeffs, i, 10)
+    le.plot_local_energies(local_energies, 200, i, 8)
+    energy_i = np.sum(local_energies) / len(local_energies)
+    energies.append(energy_i)
+
 
 # %%

two_one/differentiators.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import two_one.polynomials as poly
 
-def fd_second(stepsize, range_val, samples_num, coeffs):
+def fd_second(x_vals, wavefunction_vals, stepsize, coeffs, polynomial=True):
     """
     Approximates the second derivative of the Hermitian functions defined
     earlier with the central midpoint difference method. Truncation on the
@@ -13,24 +13,24 @@ def fd_second(stepsize, range_val, samples_num, coeffs):
     range_val - (list): The beggining and end points of the independent variable.
     level - (int): The order of the Hermite polynomial to be differentiated.
     """
-    x_0 = range_val[0]
-    x_f = range_val[-1]
-    samples = np.linspace(x_0, x_f, samples_num)
-    n = len(samples)
+    n = len(x_vals)
     func_vals = []
+    if polynomial is True:
+        for i in range(n):
+            term = poly.polynomial(x_vals[i], coeffs)
+            func_vals.append(term)
+    else:
+        func_vals = wavefunction_vals
 
-    for i in range(n):
-        term = poly.polynomial(samples[i], coeffs)
-        func_vals.append(term)
     func_vals = np.array(func_vals)
+
     sec_der_vals = (func_vals[2:] - 2 * func_vals[1:-1] + func_vals[:-2]) / (stepsize ** 2)
-    samples_inner = samples[1:-1]
-    func_inner    = func_vals[1:-1]
-    terms = -0.5 * sec_der_vals / func_inner
+    samples_inner = x_vals[1:-1]
+    func_inner = func_vals[1:-1]
 
-    return samples_inner, terms, sec_der_vals
+    return samples_inner, sec_der_vals, func_inner
 
-def fd_fourth(stepsize, range_val, samples_num, coeffs):
+def fd_fourth(x_vals, wavefunction_vals, stepsize, coeffs, polynomial=True):
     """
     Approximates the second derivative of the Hermitian functions defined
     earlier with the central midpoint difference method. Truncation on the
@@ -41,15 +41,16 @@ def fd_fourth(stepsize, range_val, samples_num, coeffs):
     range_val - (list): The beggining and end points of the independent variable.
     level - (int): The order of the Hermite polynomial to be differentiated.
     """
-    x_0 = range_val[0]
-    x_f = range_val[-1]
-    samples = np.linspace(x_0, x_f, samples_num)
-    n = len(samples)
+    n = len(x_vals)
     func_vals = []
 
-    for i in range(n):
-        term = poly.polynomial(samples[i], coeffs)
-        func_vals.append(term)
+    if polynomial is True:
+        for i in range(n):
+            term = poly.polynomial(x_vals[i], coeffs)
+            func_vals.append(term)
+    else:
+        func_vals = wavefunction_vals
+
     func_vals = np.array(func_vals)
 
     sec_der_vals = []
@@ -64,13 +65,12 @@ def fd_fourth(stepsize, range_val, samples_num, coeffs):
         sec_der_vals.append(fpp)
     sec_der_vals = np.array(sec_der_vals)
 
-    samples_inner = samples[2:-2]
+    samples_inner = x_vals[2:-2]
     func_inner = func_vals[2:-2]
-    terms = -0.5 * sec_der_vals / func_inner
 
-    return samples_inner, terms, sec_der_vals
+    return samples_inner, sec_der_vals, func_inner
 
-def fd_sixth(stepsize, range_val, samples_num, coeffs):
+def fd_sixth(x_vals, wavefunction_vals, stepsize, coeffs, polynomial=True):
     """
     Approximates the second derivative of the Hermitian functions defined
     earlier with the central midpoint difference method. Truncation on the
@@ -81,15 +81,16 @@ def fd_sixth(stepsize, range_val, samples_num, coeffs):
     range_val - (list): The beggining and end points of the independent variable.
     level - (int): The order of the Hermite polynomial to be differentiated.
     """
-    x_0 = range_val[0]
-    x_f = range_val[-1]
-    samples = np.linspace(x_0, x_f, samples_num)
-    n = len(samples)
+    n = len(x_vals)
     func_vals = []
 
-    for i in range(n):
-        term = poly.polynomial(samples[i], coeffs)
-        func_vals.append(term)
+    if polynomial is True:
+        for i in range(n):
+            term = poly.polynomial(x_vals[i], coeffs)
+            func_vals.append(term)
+    else:
+        func_vals = wavefunction_vals
+
     func_vals = np.array(func_vals)
 
     sec_der_vals = []
@@ -106,13 +107,12 @@ def fd_sixth(stepsize, range_val, samples_num, coeffs):
         sec_der_vals.append(fpp)
     sec_der_vals = np.array(sec_der_vals)
 
-    samples_inner = samples[3:-3]
+    samples_inner = x_vals[3:-3]
     func_inner = func_vals[3:-3]
-    terms = -0.5 * sec_der_vals / func_inner
 
-    return samples_inner, terms, sec_der_vals
+    return samples_inner, sec_der_vals, func_inner
 
-def fd_eighth(stepsize, range_val, samples_num, coeffs):
+def fd_eighth(x_vals, wavefunction_vals, stepsize, coeffs, polynomial=True):
     """
     Approximates the second derivative of the Hermitian functions defined
     earlier with the central midpoint difference method. Truncation on the
@@ -123,15 +123,16 @@ def fd_eighth(stepsize, range_val, samples_num, coeffs):
     range_val - (list): The beggining and end points of the independent variable.
     level - (int): The order of the Hermite polynomial to be differentiated.
     """
-    x_0 = range_val[0]
-    x_f = range_val[-1]
-    samples = np.linspace(x_0, x_f, samples_num)
-    n = len(samples)
+    n = len(x_vals)
     func_vals = []
 
-    for i in range(n):
-        term = poly.polynomial(samples[i], coeffs)
-        func_vals.append(term)
+    if polynomial is True:
+        for i in range(n):
+            term = poly.polynomial(x_vals[i], coeffs)
+            func_vals.append(term)
+    else:
+        func_vals = wavefunction_vals
+
     func_vals = np.array(func_vals)
 
     sec_der_vals = []
@@ -150,13 +151,12 @@ def fd_eighth(stepsize, range_val, samples_num, coeffs):
         sec_der_vals.append(fpp)
     sec_der_vals = np.array(sec_der_vals)
 
-    samples_inner = samples[4:-4]
+    samples_inner = x_vals[4:-4]
     func_inner = func_vals[4:-4]
-    terms = -0.5 * sec_der_vals / func_inner
 
-    return samples_inner, terms, sec_der_vals
+    return samples_inner, sec_der_vals, func_inner
 
-def fd_tenth(stepsize, range_val, samples_num, coeffs):
+def fd_tenth(x_vals, wavefunction_vals, stepsize, coeffs, polynomial):
     """
     Approximates the second derivative of the Hermitian functions defined
     earlier with the central midpoint difference method. Truncation on the
@@ -167,15 +167,16 @@ def fd_tenth(stepsize, range_val, samples_num, coeffs):
     range_val - (list): The beggining and end points of the independent variable.
     level - (int): The order of the Hermite polynomial to be differentiated.
     """
-    x_0 = range_val[0]
-    x_f = range_val[-1]
-    samples = np.linspace(x_0, x_f, samples_num)
-    n = len(samples)
+    n = len(x_vals)
     func_vals = []
 
-    for i in range(n):
-        term = poly.polynomial(samples[i], coeffs)
-        func_vals.append(term)
+    if polynomial is True:
+        for i in range(n):
+            term = poly.polynomial(x_vals[i], coeffs)
+            func_vals.append(term)
+    else:
+        func_vals = wavefunction_vals
+
     func_vals = np.array(func_vals)
 
     sec_der_vals = []
@@ -196,11 +197,10 @@ def fd_tenth(stepsize, range_val, samples_num, coeffs):
         sec_der_vals.append(fpp)
     sec_der_vals = np.array(sec_der_vals)
 
-    samples_inner = samples[5:-5]
+    samples_inner = x_vals[5:-5]
     func_inner = func_vals[5:-5]
-    terms = -0.5 * sec_der_vals / func_inner
 
-    return samples_inner, terms, sec_der_vals
+    return samples_inner, sec_der_vals, func_inner
 
 
 def analytical_second_der(x, coeffs, level, plot):
@@ -215,13 +215,13 @@ def analytical_second_der(x, coeffs, level, plot):
     current_coeffs = coeffs[level]
     n = len(current_coeffs)
     func_vals = np.polyval(current_coeffs[::-1], x)
-    
+
     sec_der_vals_exact = np.zeros_like(x, dtype=float)
     for i in range(2, n):
         sec_der_vals_exact += i * (i - 1) * current_coeffs[i] * (x ** (i - 2))
     terms = -0.5 * sec_der_vals_exact / func_vals
 
-    if plot == True:
+    if plot is True:
         plt.loglog(x, terms)
         plt.grid()
         plt.xlabel('x')

two_one/local_energy.py
@@ -1,7 +1,21 @@
+import numpy as np
+import math
 import matplotlib.pyplot as plt
 import two_one.differentiators as diff
 
-def local_energy(stepsize, range_val, samples_num, coeffs, level, method, plot=False):
+def wavefunction(x, coeffs):
+    """Return the NORMALIZED calculated wavefunction."""
+    H_n = np.polyval(coeffs, x)
+    
+    # Get quantum number n from the polynomial degree
+    n = len(coeffs) - 1  # degree of polynomial
+    
+    # NORMALIZATION CONSTANT for quantum harmonic oscillator
+    normalization = 1.0 / np.sqrt(2**n * math.factorial(n) * np.sqrt(np.pi))
+    
+    return normalization * H_n * np.exp(-x**2 / 2)
+
+def local_energy(x_vals, stepsize, coeffs, level, method):
     """
     Compute the local energy of a specific wavefunction.
 
@@ -12,20 +26,35 @@ def local_energy(stepsize, range_val, samples_num, coeffs, level, method, plot=F
     method - (bool): Whether to use fourth order truncation (==1) or second (else).
     """
     coeffs = coeffs[level]
+    wavefunction_vals = wavefunction(x_vals, coeffs)
+
     if method == 1:
-        x_vals, y_vals_first, other = diff.fd_fourth(stepsize, range_val, samples_num, coeffs)
+        x_val_calc, sec_der_vals, wf_trimmed = diff.fd_second(x_vals, wavefunction_vals, stepsize, coeffs, polynomial=False)
+    elif method == 2:
+        x_val_calc, sec_der_vals, wf_trimmed = diff.fd_fourth(x_vals, wavefunction_vals, stepsize, coeffs, polynomial=False)
+    elif method == 3:
+        x_val_calc, sec_der_vals, wf_trimmed = diff.fd_sixth(x_vals, wavefunction_vals, stepsize, coeffs, polynomial=False)
+    elif method == 4:
+        x_val_calc, sec_der_vals, wf_trimmed = diff.fd_tenth(x_vals, wavefunction_vals, stepsize, coeffs, polynomial=False)
     else:
-        x_vals, y_vals_first, other = diff.fd_second(stepsize, range_val, samples_num, coeffs)
+        x_val_calc, sec_der_vals, wf_trimmed = diff.fd_eighth(x_vals, wavefunction_vals, stepsize, coeffs, polynomial=False)
 
-    y_vals_sec = 0.5 * (x_vals ** 2)
-    l_energy = y_vals_first + y_vals_sec
-
-    if plot is True:
-        plt.plot(x_vals, l_energy)
-        plt.grid()
-        plt.xlabel('x')
-        plt.ylabel("E_l")
-        plt.title(f'Local Energy (E_l) for the wavefunction, method {method}')
-        plt.show()
+    threshold = 1e-6
+    mask = np.abs(wf_trimmed) > threshold
+    x_filtered = x_val_calc[mask]
+    sec_der_filtered = sec_der_vals[mask] 
+    wf_filtered = wf_trimmed[mask]
+    
+    l_energy = - 0.5 * sec_der_filtered / wf_filtered + 0.5 * (x_filtered ** 2)
 
     return x_vals, l_energy
+
+def plot_local_energies(energy_vals, bins, order, truncation):
+    """Plot the local energies obtained."""
+    plt.hist(energy_vals, bins=bins, density=True)
+    plt.xlim(-3000000, 3000000)
+    plt.grid()
+    plt.xlabel('x')
+    plt.ylabel("E_l")
+    plt.title(f'Local Energy (E_l) for the n={order} wavefunction, {truncation}th truncation.')
+    plt.show()